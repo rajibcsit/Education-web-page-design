ae9c6f662a787bbb025c49f9351d0b58e9a62c66
rajibcsit <18192203021@cse.bubt.edu.bd>
2023-02-27 13:00:02 +0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@/styles/rt-plugins.css";
+import "@/styles/app.css";
 
+import Script from "next/script";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Script src="../js/popper.min.js" />
+      <Script src="../js/jquery-3.6.0.min.js" />
+      <Script src="../js/rt-plugins.js" />
+      <Script src="https://unpkg.com/phosphor-icons" />
+      <Script src="https://code.iconify.design/iconify-icon/1.0.0/iconify-icon.min.js" />
+      <Script src="../js/app.js" />
+    </>
+  );
 }
